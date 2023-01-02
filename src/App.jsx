1be11ef3d2063167028472dc046853806ac021d7
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-02 13:47:00 +0300

    writting and rendering of components(composability)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+ 
+
+function Header(){
+    return(
+
+     <header>
+      <h1>I am the Title</h1>
+     </header>
+    )
+}   
+   
+
+function Content(){
+  return(
+    <main>
+      <section>I am section 1</section>  
+      <section>I am section 2</section>
+    </main>
+  )
+}
+
+
+
 
 function App() {  
   // Why React ?
@@ -6,12 +29,19 @@ function App() {
 
   // JSX--javascript XML--brought to make react declarative 
     //  turns html element to object
+    
+    // React component returns a single JSX (javascript objects) 
+    // Fragments can be used to wrap JSX   
 
+    // COMPONENTS----functions in react.Wrapped in self closing tag(<Component />).Writen in PascalCase.Returns only one element  
+    //  footer mostly wrapped in <small></small>  tag.
   return (
-    <div className="App">
-          
+    <div className="App">    
+      {/rendering of Components(composabilitty) to make web page*  */}
+         <Header />       
+         <Content />  
     </div>
-  )
+  )      
 }
 
 export default App
