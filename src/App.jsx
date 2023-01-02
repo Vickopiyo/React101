1191f78a2bddc0ae70acf55a1edb05db01c1ec7e
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-02 12:55:30 +0300

    Files and folder strucure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {  
+  // Why React ?
+  // Composabiity--combines many components to make one component
+  // Declarative--program is told what to do and does it unlike imperative where program is told what to do and how it should do it     
+
+  // JSX--javascript XML--brought to make react declarative 
+    //  turns html element to object
+
+  return (
+    <div className="App">
+          
+    </div>
+  )
+}
+
+export default App
